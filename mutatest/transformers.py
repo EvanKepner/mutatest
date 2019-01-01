94c3d6b20fe55f3f0fe1822483f044ea6dda4d96
Evan Kepner <EvanKepner@users.noreply.github.com>
2019-01-01 17:25:18 -0500

    add visit_Compare to transformers

--- mutatest/transformers.py
+++ mutatest/transformers.py
@@ -47,6 +47,7 @@ class MutateAST(ast.NodeTransformer):
         self.mutation = mutation
 
     def visit_BinOp(self, node: ast.BinOp) -> ast.AST:
+        """BinOp nodes are bit-shifts and general operators like add, divide, etc."""
         self.generic_visit(node)
 
         idx = LocIndex("BinOp", node.lineno, node.col_offset, type(node.op))
@@ -57,6 +58,49 @@ class MutateAST(ast.NodeTransformer):
             return ast.copy_location(
                 ast.BinOp(left=node.left, op=self.mutation(), right=node.right), node
             )
+
+        else:
+            LOGGER.debug("No mutations applied")
+            return node
+
+    def visit_Compare(self, node: ast.Compare) -> ast.AST:
+        """Compare nodes are ==, >= etc."""
+        self.generic_visit(node)
+
+        # taking only the first operation in the compare node
+        # in basic testing, things like (a==b)==1 still end up with lists of 1,
+        # but since the AST docs specify a list of operations this seems safer.
+        idx = LocIndex("Compare", node.lineno, node.col_offset, type(node.ops[0]))
+        self.locs.add(idx)
+
+        if idx == self.target_idx and self.mutation:
+            LOGGER.debug("Mutating idx: %s with %s", self.target_idx, self.mutation)
+
+            if len(node.ops) > 1:
+                # unlikely test case where the comparison has multiple values
+                LOGGER.debug("Multiple compare ops in node, len: %s", len(node.ops))
+                existing_ops = [i for i in node.ops]
+                mutation_ops = [self.mutation()] + existing_ops[1:]
+
+                return ast.copy_location(
+                    ast.Compare(left=node.left, ops=mutation_ops, comparators=node.comparators),
+                    node,
+                )
+
+            else:
+                # typical comparison case, will also catch (a==b)==1 as an example.
+                LOGGER.debug("Single comparison node operation")
+                new_node = ast.Compare(
+                    left=node.left, ops=[self.mutation()], comparators=node.comparators
+                )
+                LOGGER.debug("New node:\n%s", ast.dump(new_node))
+
+                return ast.copy_location(
+                    ast.Compare(
+                        left=node.left, ops=[self.mutation()], comparators=node.comparators
+                    ),
+                    node,
+                )
 
         else:
             LOGGER.debug("No mutations applied")
