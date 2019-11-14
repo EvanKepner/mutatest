998d70c7b3d5586b032e3620fee0b44e951419d6
Evan Kepner <EvanKepner@users.noreply.github.com>
2019-11-14 08:25:52 -0500

    fix library classifier

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ CLASSIFIERS = [
     "Programming Language :: Python :: 3.7",
     "Programming Language :: Python :: 3.8",
     "Topic :: Utilities",
-    "Topic :: Software Development:: Libraries",
+    "Topic :: Software Development :: Libraries",
     "Topic :: Software Development :: Libraries :: Python Modules",
     "Topic :: Software Development :: Quality Assurance",
     "Topic :: Software Development :: Testing",
